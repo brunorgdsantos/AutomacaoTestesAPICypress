f01a3b8a56647645a8b8a9ff1d43af20a8ae68cb
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-11 21:52:34 -0300

    create find user end-point

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,8 +1,7 @@
 describe("API Tests", () => {
-  it("Criar Usuario", () => {
-    const randomEmail = `teste${Cypress._.random(1000, 9999)}@qa.com.br`;
-    
-    cy.request({
+    it("Criar Usuario", () => {
+        const randomEmail = `teste${Cypress._.random(1000, 9999)}@qa.com.br`;
+        cy.request({
             method: "POST",
             url: "https://serverest.dev/usuarios",
             body: {
@@ -15,9 +14,28 @@ describe("API Tests", () => {
         .then((response) => {
             expect(response.status).to.eq(201);
             expect(response.body.message).to.eq("Cadastro realizado com sucesso");
-            Cypress.env("id", response.body._id);
+            Cypress.env('id', response.body._id);
         });
     });
 
+    it("Busca Usuario Lista", () => {
+        cy.request({
+            method: "GET",
+            url: "https://serverest.dev/usuarios",
+        })
+        .then((response) => {
+            expect(response.status).to.eq(200);
+        });
+    });
 
+    it("Busca Usuario por ID", () => {
+        cy.request({
+            method: "GET",
+            url: `https://serverest.dev/usuarios/${Cypress.env('id')}`,
+        })
+        .then((response) => {
+            expect(response.status).to.eq(200);
+        });
+    });
+    
 })
